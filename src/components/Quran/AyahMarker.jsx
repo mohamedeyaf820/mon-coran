1d77428bca9f16854fb43686d3784ff7fdc9ecea
finw <mohamedeyaf820@gmail.com>
2026-08-18 00:21:47 +0000

    Revert "fix(reader): replace QPC Hafs rosette markers with Quran.com-style circles"
    This reverts commit 3af93b0fbe81de5c8228f62e20c4e9323627f628.

--- src/components/Quran/AyahMarker.jsx
+++ src/components/Quran/AyahMarker.jsx
@@ -6,7 +6,9 @@ import {
   useAppSelector,
 } from "../../context/AppContext";
 import {
+  getNativeAyahMarker,
   normalizeFontId,
+  resolveFontFamily,
 } from "../../data/fonts";
 import { t } from "../../i18n";
 import { toArabicNumeral } from "../../utils/arabicNumerals";
@@ -36,6 +38,9 @@ export const AyahMarker = React.memo(function AyahMarker({
     fontFamily || readingPreferences.fontFamily,
     activeRiwaya,
   );
+  const markerFontFamily = resolveFontFamily(activeFont, activeRiwaya);
+
+  const markerText = getNativeAyahMarker(markerNumber, activeFont, activeRiwaya);
 
   return (
     <span
@@ -51,6 +56,7 @@ export const AyahMarker = React.memo(function AyahMarker({
       tabIndex={onClick ? 0 : undefined}
       aria-label={`Verset ${markerNumber}`}
       data-marker-font={activeFont}
+      style={{ fontFamily: markerFontFamily }}
       onClick={onClick}
       onKeyDown={
         onClick
@@ -63,7 +69,7 @@ export const AyahMarker = React.memo(function AyahMarker({
           : undefined
       }
     >
-      <span aria-hidden="true">{toArabicNumeral(markerNumber)}</span>
+      <span aria-hidden="true">{markerText}</span>
     </span>
   );
 });
